abb01c165caf27d9ead703664308feaa19c9cf67
Takehiro Takahashi <taka.mc.0509@gmail.com>
2019-04-30 16:11:55 +0900

    add background function

--- swift5-weather-app/ViewController.swift
+++ swift5-weather-app/ViewController.swift
@@ -15,13 +15,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-    
+    @IBOutlet weak var backgroundView: UIView!
+    let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundView.layer.addSublayer(gradientLayer)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setBlueGradientBackground()
+        setGrayGradientBackground()
+    }
+    
+    func setBlueGradientBackground(){
+        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
+        let bottonColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottonColor]
+    }
+    
+    func setGrayGradientBackground(){
+        let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
+        let bottonColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, bottonColor]
+    }
 }
 
